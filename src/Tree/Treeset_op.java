22ad96580a9ad0ec67f1d5e62dd8663f384c6d32
Prasanth <prasanthps8838@gmail.com>
2024-07-19 06:29:39 +0530

    commit

--- src/Tree/Treeset_op.java
+++ src/Tree/Treeset_op.java
@@ -1,10 +1,17 @@
 package Tree;
 
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Treeset_op {
     public static void main(String[] args) {
         TreeSet<String> tree=new TreeSet<>();
+        TreeMap<Integer,String> map=new TreeMap<>();
+        map.put(5,"nine");
+        map.put(4,"roy");
+        map.put(1,"manisha");
+        map.put(9,"laura");
+
         tree.add("Bob");
         tree.add("Sam");
         tree.add("Anne");
@@ -13,6 +20,7 @@ public class Treeset_op {
         for(String word:tree){
             System.out.println(word+" ");
         }
+        System.out.println(map);
     }
     // So any values we put into a treeset should be auto sorted 😃
 }

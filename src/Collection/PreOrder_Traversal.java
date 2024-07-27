7bfbb8db37fb5c0d2d77d2542ff59a48078a0114
Prasanth <prasanthps8838@gmail.com>
2024-07-27 11:27:20 +0530

    commit

--- src/Collection/PreOrder_Traversal.java
+++ src/Collection/PreOrder_Traversal.java
@@ -33,25 +33,34 @@ public class PreOrder_Traversal {
 
     }
 
-    public static void levelorder(Node root){
+    public static void levelorder(Node root,ArrayList<ArrayList<Integer>>ar){
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
 
       while(!queue.isEmpty()){
-         Node n1=queue.peek();
-         if(n1.left != null) {
-             queue.add(n1.left);
-         }
-         if(n1.right != null) {
-             queue.add(n1.right);
-         }
-         System.out.print( queue.remove().data+" ");
+          int levelSize = queue.size();
+          ArrayList<Integer> alist = new ArrayList<>();
+
+          for (int i = 0; i < levelSize; i++) {
+
+              Node n1 = queue.poll();
+              alist.add(n1.data);
+
+              if (n1.left != null) {
+                  queue.add(n1.left);
+              }
+              if (n1.right != null) {
+                  queue.add(n1.right);
+              }
+          }
+
+         ar.add(alist);
 
      }
     }
 
     public static void main(String[] args) {
-
+        ArrayList<ArrayList<Integer>>ar=new ArrayList<>();
         Node root= new Node(1);
         root.left=new Node(2);
         root.right=new Node(3);
@@ -60,13 +69,14 @@ public class PreOrder_Traversal {
         root.right.left=new Node(6);
         root.right.right=new Node(7);
         System.out.print("Pre order   : ");
-        preorder(root);
+      //  preorder(root);
         System.out.print("\nPost Order  : ");
-        postorder(root);
+     //   postorder(root);
         System.out.print("\nIn order    : ");
-        Inorder(root);
+    //    Inorder(root);
         System.out.print("\nLevel order : ");
-        levelorder(root);
+        levelorder(root,ar);
+        System.out.println(ar);
 
     }
 }

f7e6de044bed75cee1f1b69369c1390104f2640b
Prasanth210 <prasanthps8838@gmail.com>
2025-03-08 09:28:23 +0530

    commit

--- src/Dummy/One.java
+++ src/Dummy/One.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class One {
     public static void main(String[] args) {
         Scanner scan =new Scanner(System.in);
-        int size= scan.nextInt();;
+        int size= scan.nextInt();
         for(int n=1;n<=size;n++){
             System.out.print(n*n+" "+n*n*n+" ");
 

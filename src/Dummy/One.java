c4420a72e3f10fa769cd5e7330608e81aff23945
Prasanth210 <prasanthps8838@gmail.com>
2025-03-08 09:38:25 +0530

    commit

--- src/Dummy/One.java
+++ src/Dummy/One.java
@@ -6,10 +6,15 @@ public class One {
     public static void main(String[] args) {
         Scanner scan =new Scanner(System.in);
         int size= scan.nextInt();
+        int val=3;
         for(int n=1;n<=size;n++){
-            System.out.print(n*n+" "+n*n*n+" ");
-
+            System.out.print(n*n+" "+ val +" ");
+            val*=3;
         }
 
     }
 }
+
+//
+//459 500
+//549
